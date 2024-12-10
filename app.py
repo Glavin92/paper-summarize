d5e3ab25cedd3ba94c903d36f56d0a32a5781125
Glavin <glavinlobo92@gmail.com>
2024-12-11 00:45:41 +0530

    third commit

--- app.py
+++ app.py
@@ -143,4 +143,5 @@ def search():
     return render_template('index.html', faculty_publications={author_name: publications})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
